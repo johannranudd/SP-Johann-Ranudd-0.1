304292bbe5e647253843f35a209a13a4a755c97f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2020-11-17 23:55:59 +0100

    commit 2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector(".menu");
 
 
+
 hamburger.addEventListener("click", function showMenu() {
   menu.classList.toggle("show-menu");
 });

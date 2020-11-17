acf4ffd64feafe7c3f337b359b511288b3bc7011
johannranudd <33978097+johannranudd@users.noreply.github.com>
2020-11-17 16:58:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector(".menu");
+
+
+hamburger.addEventListener("click", function showMenu() {
+  menu.classList.toggle("show-menu");
+});
+
+// timeline
+
+

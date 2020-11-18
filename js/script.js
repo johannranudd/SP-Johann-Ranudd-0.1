444280c9f0bd6a92c8cd0ca4a3a47bbb29cec593
johannranudd <33978097+johannranudd@users.noreply.github.com>
2020-11-18 16:07:55 +0100

    commit 4

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector(".menu");
 
-
-
 hamburger.addEventListener("click", function showMenu() {
   menu.classList.toggle("show-menu");
 });
 
-// timeline
+// dropBtn
+
+const dropBtn = document.querySelector("#dropBtn");
+const hiddenSection = document.querySelector(".hidden-section");
 
 
+dropBtn.addEventListener("click", function () {
+    hiddenSection.classList.toggle('show-section');
+});
+
+// closeBtn
+
+const closeBtn = document.querySelector('#closeBtn');
+
+closeBtn.addEventListener('click', function () {
+  hiddenSection.classList.toggle('show-section');
+})
